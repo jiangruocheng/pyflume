0bdacfda78316e55b5b27f071e2757cb7e25b5bb
JiangRuocheng <allenjiang@digiwin.biz>
2016-10-17 14:03:29 +0800

    使用configparser管理配置文件；

--- src/pyflume.py
+++ src/pyflume.py
@@ -1,39 +1,43 @@
 #! -*- coding:utf-8 -*-
 
 import os
+import sys
 import select
 import pickle
+import signal
+import logging
+import threading
 import traceback
-import configuration as conf
 
 from time import sleep
 from collector import Collector
+from select import KQ_FILTER_SIGNAL, KQ_FILTER_READ, KQ_EV_ADD
 
 
 class Pyflume(object):
 
     def __init__(self, config):
-        self._config = config
-        self._pickle_path = config.pickle_path
-        self._max_read_line = config.max_read_line
+        self._pickle_file = config.get('TEMP', 'PICKLE_FILE')
+        self._max_read_line = int(config.get('POOL', 'MAX_READ_LINE'))
         self.pickle_handler = None
         self._pickle_data = None
         self._handlers = list()
-        self._time_seed = 0.2
+        self._pool_path = config.get('POOL', 'POOL_PATH')
+        self.logger = logging.getLogger(config.get('LOG', 'LOG_HANDLER'))
 
     def _load_pickle(self):
-        if not os.path.exists(self._pickle_path):
+        if not os.path.exists(self._pickle_file):
             try:
-                self.pickle_handler = open(self._pickle_path, 'w+')
-            except IOError, e:
-                conf.LG.error(e)
+                self.pickle_handler = open(self._pickle_file, 'w+')
+            except IOError:
+                self.logger.error(traceback.format_exc())
                 exit(-1)
 
-        self.pickle_handler = open(self._pickle_path, 'r+')
+        self.pickle_handler = open(self._pickle_file, 'r+')
         try:
             self._pickle_data = pickle.load(self.pickle_handler)
         except EOFError:
-            conf.LG.warning('No pickle data exits.')
+            self.logger.warning('No pickle data exits.')
             self._pickle_data = dict()
 
     def _get_log_data_by_handler(self, handler):
@@ -42,7 +46,7 @@ class Pyflume(object):
         try:
             _offset = self._pickle_data[_file_name]
         except KeyError:
-            conf.LG.warning('Cant find "%s" in pickles.' % _file_name)
+            self.logger.warning('Cant find "%s" in pickles.' % _file_name)
             _offset = 0
         handler.seek(_offset)
         _data = handler.readlines(self._max_read_line)
@@ -51,51 +55,78 @@ class Pyflume(object):
         else:
             return None
 
-    @staticmethod
-    def get_handlers(conf):
+    def get_handlers(self):
         _handlers = list()
-        _files = os.listdir(conf.pool_path)
+        _files = os.listdir(self._pool_path)
         for _file in _files:
             if 'COMPLETED' != _file.split('.')[-1]:
-                if os.path.isfile(conf.pool_path + '/' + _file):
+                if os.path.isfile(self._pool_path + _file):
                     try:
-                        _handler = open(conf.pool_path + '/' + _file, 'r')
+                        _handler = open(self._pool_path + _file, 'r')
                         _handlers.append(_handler)
                     except IOError:
-                        conf.LG.error(traceback.format_exc())
+                        self.logger.error(traceback.format_exc())
                         return []
 
         return _handlers
     
     def run(self):
 
+        # _thread_move = threading.Thread(target=self.monitor_file_move, name='monitor_file_move')
+        _thread_content = threading.Thread(target=self.monitor_file_content, name='monitor_file_content')
+
+        # _thread_move.start()
+        _thread_content.start()
+
+        _thread_content.join()
+        # _thread_move.join()
+
+    def monitor_file_move(self):
+
         while True:
-            self._handlers = self.get_handlers(self._config)
+            sleep(10)
+            break
+
+    def monitor_file_content(self):
+
+        while 1:
+            break_flag = True
+            self._handlers = self.get_handlers()
             self._load_pickle()
             kq = select.kqueue()
             _monitor_list = list()
             _monitor_dict = dict()
             for _handler in self._handlers:
                 _monitor_list.append(
-                    select.kevent(_handler.fileno(), filter=select.KQ_FILTER_READ, flags=select.KQ_EV_ADD)
+                    select.kevent(_handler.fileno(), filter=KQ_FILTER_READ, flags=KQ_EV_ADD)
                 )
                 _monitor_dict[_handler.fileno()] = _handler
-
-            # 此时另一个线程负责监控文件的变动,例如新文件的移入,另一个线程会发送信号终止这个循环,重新遍历获取所有文件的句柄.
-            while True:
-                revents = kq.control(_monitor_list, 2)
-                for event in revents:
-                    if event.filter == select.KQ_FILTER_READ:
-                        _in_process_file_handler = _monitor_dict[event.ident]
-                        _data = self._get_log_data_by_handler(_in_process_file_handler)
-                        if not _data:
-                            continue
-                        _result_flag = Collector().process_data(_in_process_file_handler, _data)
-                        if _result_flag:
-                            # 数据已成功采集,更新offset
-                            self._pickle_data[_in_process_file_handler.name] = _in_process_file_handler.tell()
-                            # 从文件读取_pickle_data时offset已经改变,现在重置到0
-                            self.pickle_handler.seek(0)
-                            pickle.dump(self._pickle_data, self.pickle_handler)
-
-            self.pickle_handler.close()
+            _monitor_list.append(
+                select.kevent(signal.SIGUSR1, filter=KQ_FILTER_SIGNAL, flags=KQ_EV_ADD)
+            )
+            try:
+                # 此时另一个线程负责监控文件的变动,例如新文件的移入,另一个线程会发送信号终止这个循环,重新遍历获取所有文件的句柄.
+                while break_flag:
+                    revents = kq.control(_monitor_list, 3)
+                    for event in revents:
+                        if event.filter == select.KQ_FILTER_READ:
+                            _in_process_file_handler = _monitor_dict[event.ident]
+                            _data = self._get_log_data_by_handler(_in_process_file_handler)
+                            if not _data:
+                                continue
+                            _result_flag = Collector().process_data(_in_process_file_handler, _data)
+                            if _result_flag:
+                                # 数据已成功采集,更新offset
+                                self._pickle_data[_in_process_file_handler.name] = _in_process_file_handler.tell()
+                                # 从文件读取_pickle_data时offset已经改变,现在重置到0
+                                self.pickle_handler.seek(0)
+                                pickle.dump(self._pickle_data, self.pickle_handler)
+                        elif event.filter == select.KQ_FILTER_SIGNAL:
+                            if event.ident == signal.SIGUSR1:
+                                self.logger.info('捕捉到信号SIGUSR1,重新获取文件句柄')
+                                break_flag = False
+            except Exception:
+                self.logger.error(traceback.format_exc())
+                sys.exit(-1)
+            finally:
+                self.pickle_handler.close()
--- src/run.py
+++ src/run.py
@@ -2,8 +2,12 @@
 
 import os
 import sys
+import signal
+import logging
 import argparse
-import configuration
+import configparser
+
+from logging.handlers import TimedRotatingFileHandler
 
 from pyflume import Pyflume
 
@@ -12,27 +16,33 @@ _basedir = os.path.abspath(os.path.dirname(__file__))
 if _basedir not in sys.path:
     sys.path.insert(0, _basedir)
 
-
-
-
-
 if __name__ == '__main__':
 
+    signal.signal(signal.SIGUSR1, lambda: 0)
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--configure', help='给定配置文件路径,导入配置')
     args = parser.parse_args()
     if args.configure:
         if os.path.exists(args.configure):
             # 导入配置文件
-            _config = configuration.Configuration(args.configure)
+            config = configparser.ConfigParser()
+            config.read(args.configure)
         else:
             print 'No configure file exists.'
     else:
         # 使用默认配置
-        _config = configuration.Configuration()
+        print 'please import configure file.'
+        exit()
 
-    _config.set_app_log_config()
+    log_path = config.get('LOG', 'LOG_FILE')
+    handler = TimedRotatingFileHandler(log_path, "midnight", 1)
+    formatter = '%(asctime)s - %(filename)s:%(lineno)s - %(name)s - %(message)s'
+    handler.setFormatter(logging.Formatter(formatter))
+    level = logging.DEBUG
+    logger = logging.getLogger(config.get('LOG', 'LOG_HANDLER'))
+    logger.setLevel(level)
+    logger.addHandler(handler)
 
-    pyflume = Pyflume(_config)
+    pyflume = Pyflume(config)
     pyflume.run()
 
