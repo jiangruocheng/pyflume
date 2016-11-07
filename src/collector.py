00310335b6a81fa10994c9b8644b6619045385c8
JiangRuocheng <allenjiang@digiwin.biz>
2016-11-07 17:55:56 +0800

    bug修复，解决无法向kakfa发送数据问题，完善退出机制；

--- src/collector.py
+++ src/collector.py
@@ -1,9 +1,9 @@
 #! -*- coding:utf-8 -*-
 
-import os
 import sys
 import signal
 import logging
+import traceback
 
 from kafka import KafkaProducer
 from kafka.errors import KafkaError
@@ -29,7 +29,10 @@ class CollectorProxy(object):
         chn = self.channel()
         while not self.exit_flag:
             _msg = chn.get()
+            if isinstance(_msg, str) and 'stop' == _msg:
+                continue
             _target_collector = self.collectors.get(_msg['collector'])
+            self.log.info(_msg)
             _target_collector.process_data(_msg)
 
     def run(self, channel=None):
@@ -63,20 +66,26 @@ class KafkaCollector(Collector):
     def __init__(self, config, section):
         super(KafkaCollector, self).__init__(config)
         self.log.debug(config.get(section, 'SERVER'))
-        self.producer = KafkaProducer(bootstrap_servers=[config.get(section, 'SERVER')])
+        self.config= config
+        self.section = section
         self.topic = config.get(section, 'TOPIC')
+        self.producer = None
 
     def process_data(self, msg):
         _data = msg['filename'] + ': ' + msg['data']
         self.log.debug(msg['collector'] + _data)
 
-        future = self.producer.send(self.topic, _data.encode('utf-8'))
+        self.producer = KafkaProducer(bootstrap_servers=self.config.get(self.section, 'SERVER'))
+
+        future = self.producer.send(self.topic, _data)
         # Block for 'synchronous' sends
         try:
             record_metadata = future.get(timeout=10)
         except KafkaError:
             # Decide what to do if produce request failed...
-            self.log.exception()
+            self.log.error(traceback.format_exc())
             return None
+        finally:
+            self.producer.close()
 
         return record_metadata.topic, record_metadata.partition, record_metadata.offset
