f8c094af42a862a67fb9a62d6260cc12232d9fc7
JiangRuocheng <allenjiang@digiwin.biz>
2016-11-24 14:38:44 +0800

    memory 未消费message持久化；

--- pyflumes/channels/memery_channel.py
+++ pyflumes/channels/memery_channel.py
@@ -1,11 +1,25 @@
 #! -*- coding:utf-8 -*-
 
+import os
+import pickle
+
 from multiprocessing import Queue
+from multiprocessing.queues import Empty
 
 
 class MemoryChannel(object):
     def __init__(self, config, section):
         self.queue = Queue()
+        self.message_backup = config.get(section, 'MESSAGE_BACKUP')
+        self.fetch_back_up_data()
+
+    def fetch_back_up_data(self):
+        if not os.path.exists(self.message_backup):
+            return None
+        with open(self.message_backup, 'r') as f:
+            _list = pickle.load(f)
+        for one in _list:
+            self.put(one)
 
     def get(self, timeout=None):
 
@@ -14,3 +28,13 @@ class MemoryChannel(object):
     def put(self, data, timeout=30):
 
         self.queue.put(data, timeout=timeout)
+
+    def __del__(self):
+        _list = list()
+        while True:
+            try:
+                _list.append(self.get(timeout=1))
+            except Empty:
+                break
+        with open(self.message_backup, 'w') as f:
+            pickle.dump(_list, f)
